1e247eb4c1376983b0762d1a3299c74dd8f03de4
oneirocosm <software@oneirocosm.com>
2024-04-19 00:09:13 -0700

    feat: add border that shrinks and grows with sound
    This adds a border around the focused video that will shrink and grow as
    the sound is quieter and louder. It also removes the mute capability
    because it forced a reload. But that will be added back later.

--- src/extension/index.ts
+++ src/extension/index.ts
@@ -1,7 +1,74 @@
 import type NodeCG from '@nodecg/types';
 import type { PlayerData } from '../types/playerdata';
+import WebSocket from 'ws';
 
-module.exports = function (nodecg: NodeCG.ServerAPI) {
+let socket: WebSocket;
+let connecting: any = undefined;
+let failedCount = 0;
+let callbackID = Math.random() * 1000;
+function connect() {
+	clearTimeout(connecting)
+	if (socket) {
+		if (socket.readyState === socket.OPEN) { return; }
+		try {
+			socket.close();
+		} catch (e) { };
+	}
+
+	socket = new WebSocket("wss://api.vdo.ninja:443");
+
+	socket.onclose = function () {
+		failedCount += 1;
+		clearTimeout(connecting);
+		connecting = setTimeout(function () { connect(); }, 100 * (failedCount - 1))
+	}
+
+	socket.onerror = function (e) {
+		console.error(e);
+		failedCount += 1;
+		clearTimeout(connecting);
+		connecting = setTimeout(function () { connect(); }, 100 * (failedCount - 1))
+	}
+
+	socket.onopen = function () {
+		failedCount = 0;
+		try {
+			socket.send(JSON.stringify({ "join": "8ss5J7EyNC" }))
+		} catch (e) {
+			connecting = setTimeout(function () { connect(); }, 1)
+		}
+		//socket.send(JSON.stringify({ "join": "nbklri9t0k5tfg" }))
+		//socket.send(JSON.stringify({ "join": "45igret40tj9" }))
+	}
+
+	socket.addEventListener('message', function (event) {
+		if (event.data) {
+			var data = JSON.parse(event.data as string);
+			//console.log(data);
+			if ("callback" in data) {
+				if (data.callback.value == callbackID) {
+					console.log("Received response. See developer console for the message.");
+				} else {
+					console.log(`other: ${JSON.stringify(data)}`)
+				}
+			}
+		}
+	});
+}
+
+async function requestStats() {
+	let count = 0;
+	while (true) {
+		if (count < 10 && socket.readyState == socket.OPEN) {
+			socket.send(JSON.stringify({ "action": "getDetails", "cid": callbackID }));
+			count += 1;
+		}
+		await new Promise(r => setTimeout(r, 2000));
+	}
+}
+
+
+module.exports = async function (nodecg: NodeCG.ServerAPI) {
 	nodecg.log.info("Hello, from your bundle's extension!");
 	nodecg.log.info("I'm where you put all your server-side code.");
 	nodecg.log.info(
@@ -20,4 +87,10 @@ module.exports = function (nodecg: NodeCG.ServerAPI) {
 	const player4 = nodecg.Replicant('player4') as unknown as NodeCG.ServerReplicantWithSchemaDefault<PlayerData>;
 	const selectedAudio = nodecg.Replicant('selectedAudio', { defaultValue: "none" });
 
+	//connect()
+	//requestStats()
 };
+
+async function testApi() {
+
+}
--- src/graphics/components/GameCamera.tsx
+++ src/graphics/components/GameCamera.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useReplicant } from '@nodecg/react-hooks'
 import styled from 'styled-components'
 import { PlayerData } from '../../types/playerdata';
+import { COLORS } from '../assets/constants';
+import parse from 'url-parse';
 
 const ScreenIframe = styled.iframe`
     background-color: #222222;
@@ -24,17 +26,64 @@ type GameCameraProps = {
 export default function GameCamera(props: GameCameraProps) {
     const [player] = useReplicant<PlayerData>(props.id);
     const [selectedAudio] = useReplicant<string>("selectedAudio")
-    const setMute = React.useCallback(() => {
-        if (selectedAudio === props.id) {
-            return ""
+    const [loudness, setLoudness] = React.useState(0);
+
+    const getGuestId = React.useCallback((): string => {
+        let url = parse(player?.gameSource ?? "", true);
+        return url.query.view as string
+    }, [player?.gameSource]);
+
+    React.useEffect(() => {
+        const iframe = document.getElementById(`${props.id}-gamecamera`) as HTMLIFrameElement;
+        let timeout: ReturnType<typeof setTimeout>
+
+        const requestLoudness = () => {
+            if (!iframe || !iframe.contentWindow) {
+                return;
+            }
+            iframe.contentWindow.postMessage({ "getLoudness": true }, "*");
+            timeout = setTimeout(() => {
+                console.log("timeout")
+                requestLoudness()
+            }, 100)
         }
-        return "&mutespeaker=1"
-    }, [selectedAudio]);
+
+        const handler = (event: MessageEvent<any>) => {
+            console.log(JSON.stringify(event.data));
+            if ("loudness" in event.data) {
+                const loudnessVal = Number(event.data.loudness[getGuestId()]);
+                const minSat = 5;
+                const maxSat = 18;
+                const minBorder = 2;
+                const maxBorder = 7;
+                const scaledLoudness = minBorder + (loudnessVal - minSat) * (maxBorder - minBorder) / (maxSat - minSat);
+                const boundedLoudness = Math.min(Math.max(scaledLoudness, minBorder), maxBorder)
+                setLoudness(boundedLoudness);
+            }
+            clearTimeout(timeout)
+            timeout = setTimeout(() => {
+                requestLoudness()
+            }, 10)
+        }
+
+        console.log("foo")
+        window.addEventListener("message", handler);
+
+        return () => {
+            clearTimeout(timeout)
+            window.removeEventListener("message", handler);
+        }
+    }, [getGuestId]);
+
     return (
-        <ScreenContainer>
+        <ScreenContainer style={{
+            boxSizing: "border-box",
+            boxShadow: `${selectedAudio === props.id ? `0 0 0 ${loudness}px ${COLORS.MOONSHOT_CORE_PINK}` : ""}`,
+        }}>
             <ScreenIframe
                 allow="autoplay;camera"
-                src={`${player?.gameSource}${setMute()}&cleanoutput=1&fullscreen=1`}
+                id={`${props.id}-gamecamera`}
+                src={`${player?.gameSource}&cleanoutput=1&fullscreen=1&style=3&meterstyle=4`}
                 style={{
                     transformOrigin: "50% 91%",
                     transform: `scale(${player?.gameScale ?? 1.0})`,

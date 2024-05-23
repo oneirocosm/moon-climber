049439f9610fa9b88db2ca27a8472d90220e0e07
Sylvia Crowe <software@oneirocosm.com>
2024-05-23 10:48:44 +0000

    feat: integrate with nodecg-tiltify

--- src/extension/index.ts
+++ src/extension/index.ts
@@ -4,7 +4,42 @@ import parse from 'url-parse';
 import { VdoNinjaAudio } from './vdo-ninja-audio';
 import { PlayerServer } from './player-server';
 import { CelesteEvent } from '../types/celesteevent';
+import { selectEvent } from './dono-effect';
+import { TiltifyEvent } from '../types/tiltify-event';
 
+interface RewardClaim {
+	id: string;
+	reward_id: string;
+	quantity: number;
+}
+
+interface Donation {
+	amount: { currency: string, value: string };
+	campaign_id: string;
+	cause_id: string;
+	completed_at: string;
+	created_at: string;
+	donor_comment?: string;
+	donor_name?: string;
+	fundraising_event_id: string;
+	id: string;
+	legacy_id: number;
+	poll_id?: string;
+	poll_option_id: string;
+	reward_custom_question?: string;
+	reward_id?: string;
+	reward_claims?: Array<RewardClaim>;
+	sustained: boolean;
+	target_id?: string;
+	team_event_id: string;
+	shown: boolean;
+	read: boolean;
+}
+
+type TiltifyRewardType = {
+	player: string;
+	type: string;
+}
 
 function getPlayerKey(player: PlayerData): string {
 	if (!player) {
@@ -33,6 +68,17 @@ module.exports = async function (nodecg: NodeCG.ServerAPI) {
 
 	const selectedAudio = nodecg.Replicant('selectedAudio', { defaultValue: "none" });
 	const hostUrl = nodecg.Replicant('hostUrl', { defaultValue: "" });
+
+	const tiltifyRewards: Record<string, TiltifyRewardType> = {};
+	tiltifyRewards[nodecg.bundleConfig.p1Pos as string] = { player: "player1", type: "positive" };
+	tiltifyRewards[nodecg.bundleConfig.p1Neg as string] = { player: "player1", type: "negative" };
+	tiltifyRewards[nodecg.bundleConfig.p1Kill as string] = { player: "player1", type: "kill" };
+	tiltifyRewards[nodecg.bundleConfig.p2Pos as string] = { player: "player2", type: "positive" };
+	tiltifyRewards[nodecg.bundleConfig.p2Neg as string] = { player: "player2", type: "negative" };
+	tiltifyRewards[nodecg.bundleConfig.p2Kill as string] = { player: "player2", type: "kill" };
+	tiltifyRewards[nodecg.bundleConfig.p3Pos as string] = { player: "player3", type: "positive" };
+	tiltifyRewards[nodecg.bundleConfig.p3Neg as string] = { player: "player3", type: "negative" };
+	tiltifyRewards[nodecg.bundleConfig.p3Kill as string] = { player: "player3", type: "kill" };
 
 	const playerIds = ["player1", "player2", "player3", "player4"];
 	const playerReps: Map<string, NodeCG.ServerReplicant<PlayerData>> = new Map();
@@ -72,6 +118,32 @@ module.exports = async function (nodecg: NodeCG.ServerAPI) {
 		if (!playerConnection) {
 			return;
 		}
+		console.log("sending ", data);
 		playerConnection.send(JSON.stringify(data));
+	});
+
+	const donations = nodecg.Replicant('donations', 'nodecg-tiltify') as unknown as NodeCG.ServerReplicant<Array<Donation>>;
+	donations.on("change", (newVal, oldVal) => {
+		if (!newVal) return;
+		const newest = newVal[newVal.length - 1];
+		console.log("donation ", newest);
+		const chosen = newest?.donor_comment ?? "";
+		newest.reward_claims?.forEach((reward) => {
+			const rewardId = reward.reward_id ?? "";
+			console.log("reward id ", rewardId);
+			const rewardType = tiltifyRewards[rewardId];
+			console.log("reward type ", rewardType);
+			if (rewardType === undefined) {
+				return;
+			}
+			let code = selectEvent(chosen, rewardType.type);
+			console.log("game code ", code);
+			let celesteEvent: CelesteEvent = {
+				playerId: rewardType.player,
+				code: code,
+			}
+			console.log("event ", celesteEvent);
+			nodecg.sendMessage("celesteEvent", celesteEvent);
+		});
 	});
 };
